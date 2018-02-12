95ceb2c610c882dee7b00302bd6cad5baa2b7831
jstack123 <league@WTS20.attlocal.net>
2018-02-11 17:32:13 -0800

    continue lovely palindromes class

--- src/data_structures/Palindromes.java
+++ src/data_structures/Palindromes.java
@@ -2,9 +2,17 @@ package data_structures;
 
 public class Palindromes {
 	public boolean isPalindrome(String word) {
-		for (int i = 0; i < instance.length; i++) {
-			
+		
+		for (int i = 0; i < word.length(); i++) {
+			char last = word.charAt(word.length()-1-i); 
+			char first = word.charAt(i);
+			char last2 = Character.toLowerCase(last);
+			char first2 = Character.toLowerCase(first);
+			if (last2 != first2 ) {
+				return false;
+			} 
 		}
+		return true;
 	}
 	
 }
